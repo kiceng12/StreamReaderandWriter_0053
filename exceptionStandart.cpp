ecb28e9a102d1bb96ecab608fea69259a70dbf02
kiceng12 <sukmahendrawan12@gmail.com>
2024-06-03 15:19:38 +0700

    last commit

--- exceptionStandart.cpp
+++ exceptionStandart.cpp
@@ -5,7 +5,21 @@
 //untuk obyek array yang akan digunakaan
 using namespace std;
 
-int main()
+int main ()
 {
-
+    cout << "Awal Program" << endl; //penanda 1:...
+    try {
+        array<int, 3> data = { 8, 4, 2};
+        //pesan array integer 3 elemen
+        cout<<data.at(5)<<endl;
+    }
+    catch (exception& e) {
+        //penangkap menggunakan obyek exception
+        cout << e.what() << endl;
+        /*akan dieksekusi karna array data hanya memiliki 3 elemen*/
+        
+    }
+    cout << "Baris Program Yang terakhir" << endl;
+    /*penanda 2: bahwa program berjalan tanpa berhenti meskipun terjadi kesalahan*/
+    return 0;
 }
