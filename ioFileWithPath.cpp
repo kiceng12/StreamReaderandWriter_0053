a14aa2cbf220fe4c401bf8c24e26a83594012705
kiceng12 <sukmahendrawan12@gmail.com>
2024-06-03 14:53:32 +0700

    menulis while dengan isi cout, getline, if

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -10,10 +10,27 @@ int main() {
     cout << "Masukan Nama File :  ";
     cin >> NamaFile;
 
-    //membuka file dalam mode menulis.
+    // membuka file dalam mode menulis.
     ofstream outfile;
-    // menunjuk sebuah nama file
-    outfile.open(NamaFile + "")
+    // menunjuk ke sebuah nama file
+    outfile.open(NamaFile + ".txt", ios::out);
+
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+    //unlimited loop untuk menulis
+    while (true) {
+        cout << "- ";
+        // mendapatkan setiap karakter dalam satu baris
+        getline(cin, baris);
+        //loop akan berhenti jika anda memasukan karakter q
+        if (baris == "q") break;
+        //menulis dan memasukan nilai dari 'baris' ke dalam file
+        outfile << baris << endl;
+    }
+    //selesai dalam menulis sekarang tutup file nya
+    outfile.close();
+
+
 }
 
 
