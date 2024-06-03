dcdee96eabc480ae4c87e3939bdf3c5018961c1f
kiceng12 <sukmahendrawan12@gmail.com>
2024-06-03 15:14:04 +0700

    menulis int main

---
+++ exceptionStandart.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <exception>
+//untuk obyek exception yang akan digunakan
+#include <array>
+//untuk obyek array yang akan digunakaan
+using namespace std;
+
+int main()
+{
+
+}
