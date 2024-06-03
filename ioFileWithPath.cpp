71d9ffa0642175432656d28929320d61093c750f
kiceng12 <sukmahendrawan12@gmail.com>
2024-06-03 14:46:57 +0700

    menulis cout dan cin

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -6,7 +6,15 @@ using namespace std;
 int main() {
     string baris;
     string NamaFile;
-    
+
+    cout << "Masukan Nama File :  ";
+    cin >> NamaFile;
+
+    //membuka file dalam mode menulis.
+    ofstream outfile;
+    // menunjuk sebuah nama file
+    outfile.open(NamaFile + "")
 }
+
 
     
