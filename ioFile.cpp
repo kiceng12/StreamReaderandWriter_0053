0be626048f2c31c5b9ab64050244f63360d1c1ab
kiceng12 <sukmahendrawan12@gmail.com>
2024-06-03 14:27:48 +0700

    menulis while, cout, getline,if,outfile

--- ioFile.cpp
+++ ioFile.cpp
@@ -14,7 +14,13 @@ int main() {
 
     //unlimited loop untuk menulis
     while(true) {
-        cout
+        cout << "- ";
+        //mendapatkan setiap karakter dalam satu baris
+        getline(cin, baris);
+        //loop akan berhenti jika anda memasukan karakter q
+        if (baris == "q") break;
+        // menulis dan memasukan nilai dari 'baris' ke dalam file
+        outfile << baris << endl;
     }
 
 }
