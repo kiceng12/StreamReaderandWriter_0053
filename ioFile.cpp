ac4e7d307a134a56c16ed4231f19b54edc8a1fc1
kiceng12 <sukmahendrawan12@gmail.com>
2024-06-03 14:24:00 +0700

    menulis string baris dan ofstream

--- ioFile.cpp
+++ ioFile.cpp
@@ -4,6 +4,18 @@
 using namespace std;
 
 int main() {
-    
+    string baris;
+    // membuka file dalam mode menulis.
+    ofstream outfile;
+    // menunjuk ke subuah nama file
+    outfile.open("contohfile.txt");
+
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+    //unlimited loop untuk menulis
+    while(true) {
+        cout
+    }
+
 }
 
